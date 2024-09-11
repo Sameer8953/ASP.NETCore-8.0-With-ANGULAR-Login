069e91c5f26f536ab0c582cbc388e4c1ef8a196b
sameer8953 <sameer.ahmadid8953@gmail.com>
2024-09-12 03:31:08 +0530

    AngularWithAsp.NETCoreIdentity

---
+++ src/app/user/registration/registration.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './registration.component.html',
+  styles: ``
+})
+export class RegistrationComponent {
+  constructor(public formBuilder: FormBuilder){}
+  isSubmitted:boolean = false;
+
+
+  passwordMatchValidator : ValidatorFn = (control:AbstractControl):null =>{
+    const password = control.get('password')
+    const confirmPassword = control.get('confirmPassword')
+
+    if(password && confirmPassword && password.value != confirmPassword.value)
+      confirmPassword?.setErrors({passwordMismatch : true})
+    else
+       confirmPassword?.setErrors(null)
+    
+    return null;
+  }
+
+
+  form = this.formBuilder.group({
+    fullName : ['', Validators.required],
+    email : ['', [Validators.required,Validators.email]],
+    password : ['',
+      [
+        Validators.required,
+         Validators.minLength(6),
+         Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/)]],
+    confirmPassword : [''],
+  },{validators:this.passwordMatchValidator})
+
+
+
+// code this submit query...
+  onSubmit(){
+    this.isSubmitted = true;
+    console.log(this.form.value);
+  }
+
+  hasDisplayableError(controlName: string):Boolean {
+const control = this.form.get(controlName);
+    return Boolean(control?.invalid) &&
+    (this.isSubmitted || Boolean(control?.touched) ) 
+  }
+  
+}
